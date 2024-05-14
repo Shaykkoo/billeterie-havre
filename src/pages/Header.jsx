dc7f91ff9035034d1c3a0feb3951bca6416507c1
Fabriciooo <fabricerob@yahoo.fr>
2024-05-14 09:51:54 +0200

    billeterie

---
+++ src/pages/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Container from "../components/Container";
+import Logo from "../assets/logos/Logo.png";
+import Hero from "../components/Hero";
+import Button from "../components/Button";
+import Background from "../assets/images/panorama_lehavre.jpg";
+import Typography from "../components/Typography";
+export default function Header() {
+  return (
+    <div>
+      <div className="relative py-28 bg-blue-100">
+        <img
+          className="absolute w-full h-full opacity-80 object-cover z-0 top-0 left-0"
+          src={Background}
+          alt=""
+        />
+        <Container>
+          <div className="flex flex-row justify-between ">
+            <div className=" flex flex-row z-20">
+              <img className="absolute" src={Logo} alt="" />
+              <Typography
+                variant="title"
+                display="white"
+                className=" pl-14 font-thin"
+              >
+                LE HAVRE
+              </Typography>
+            </div>
+            <div className="flex flex-row gap-5 z-20">
+              <Button color="primary">Se Connecter</Button>
+              <Button color="secondary">S'inscrire</Button>
+            </div>
+          </div>
+        </Container>
+      </div>
+      <Hero />
+    </div>
+  );
+}
