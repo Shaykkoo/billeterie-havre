a67d33ce506f8e79c1973d02d476611d76e97195
1804rbt <fabricerob@yahoo.fr>
2024-05-15 17:17:21 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import "./App.css";
-import Header from "./pages/Header";
-import Hero from "./components/Hero";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,35 @@
-const Button = ({ children, className, color, theme }) => {
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({ children, className, color, theme, link, label }) => {
   let background;
   switch (color) {
     case "primary":
       background = "bg-sky-600 hover:bg-sky-800";
       break;
     case "secondary":
-      background = "text-slate-700 hover:text-white bg-white hover:bg-sky-950";
+      background = "text-slate-800 hover:text-white bg-white hover:bg-sky-950";
       break;
     default:
-      background = "bg-green-500";
+      background = "none";
   }
+
+  // Si le prop 'lien' est fourni, utilisez une balise <a> pour l'ouvrir dans une nouvelle page
+  const ButtonComponent = link ? "a" : "button";
+
   return (
     <div>
-      <button
+      <ButtonComponent
+        href={link} // Utilisez 'href' si 'lien' est fourni
+        to={link} // Utilisez 'to' si 'lien' est fourni pour la navigation interne
         className={`${className} ${background} rounded-md mt-6 px-4 py-3 uppercase shadow-xl text-white 
              text-xs md:text-sm font-medium animate duration-500 `}
       >
-        {children}
-      </button>
+        {label || children}{" "}
+        {/* Utilisez le prop 'label' s'il est défini, sinon utilisez 'children' */}
+      </ButtonComponent>
     </div>
   );
 };
+
 export default Button;
